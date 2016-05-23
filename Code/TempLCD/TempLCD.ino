c6ef0e2aa43820747eb8e92cb1c79ded226cfdad
Miro Markaravanes <miro.markaravanes@gmail.com>
2016-05-23 00:53:16 -0700

    Non-blocking delays + commenting

--- Code/TempLCD/TempLCD.ino
+++ Code/TempLCD/TempLCD.ino
@@ -1,6 +1,12 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// Non-blocking Delay Functions
+// https://www.element14.com/community/groups/arduino/blog/2014/06/05/a-non-blocking-delay#comment-36267
+#define DELAY_DECLARE(name) volatile unsigned long delay_variable_##name  
+#define DELAY_SET(name, time) delay_variable_##name = millis() + time  
+#define DELAY_DONE(name) (delay_variable_##name <= millis())  
+
 // Stepper Motor Pins
 #define MS1 0
 #define MS2 1
@@ -32,6 +38,9 @@ DallasTemperature sensors(&oneWire);
 bool isGoingUp = false;
 bool isGoingDown = false;
 
+DELAY_DECLARE(heating);
+DELAY_DECLARE(pouring);
+
 void setup()
 {  
   Serial.begin(9600);
@@ -50,9 +59,13 @@ void setup()
   pinMode(WATER_PUMP, OUTPUT);
 
   // Initialize Hardware Fast PWM on Timer2
+  // http://playground.arduino.cc/Main/TimerPWMCheatsheet
   TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21) ; 
   TCCR2B = 0;
-  OCR2B = 25  ;
+  OCR2B = 25;
+  
+  // Print status to serial
+  Serial.println("{\"status\": \"pre-heating\"}");
 
   // Pre-heat the heater
   digitalWrite(WATER_PUMP, LOW);
@@ -60,8 +73,6 @@ void setup()
   delay(3000);
   digitalWrite(WATER_PUMP, HIGH);
 }
-
-int dummy = 0;
 
 void loop()
 { 
@@ -85,25 +96,43 @@ void loop()
     isGoingUp = false;
     TCCR2B = 0;
   }
-
+  
   // Send command to get temperatures
   sensors.requestTemperatures();
+  // Retrieve the current water temperature
   double temperature = sensors.getTempCByIndex(0);
-
-  // Check if we need to stop pumping water to let
-  // the heater warm up
-  float error = temperature - TARGET_TEMP;
-  if(abs(error) > 5) {
+  
+  // Use non-blocking heating to avoid locking up the piston mechanism and the other functionalities
+  if(DELAY_DONE(heating)) {
+  	if(DELAY_DONE(pouring) {
+  	  // Check if we need to stop pumping water to let
+  	  // the heater warm up
+  	  float error = temperature - TARGET_TEMP;
+  	  if(abs(error) > 5) {
+  	    // Turn off the water pump and let the heater run for 5 seconds
+  	    // Schedule the pump to run for 7 seconds after the heating
+        digitalWrite(WATER_PUMP, LOW);
+		DELAY_SET(heating, 5000);
+		DELAY_SET(pouring, 12000);
+      } else {
+        // Temperature is within the limits
+        // Keep the pump on
+        digitalWrite(WATER_PUMP, HIGH);
+      }
+  	} else {
+  	  // Heating is finished, but pumping water hasn't yet
+  	  // Keep the pump on
+      digitalWrite(WATER_PUMP, HIGH);
+  	}
+  } else {
+    // Heating hasn't finished yet
+    // Keep the pump off
     digitalWrite(WATER_PUMP, LOW);
-    delay(5000);
-    digitalWrite(WATER_PUMP, HIGH);
-    delay(7000);
   }
-  else
-    digitalWrite(WATER_PUMP, HIGH);
 
   // Write data to serial for GUI / Debugging
-  Serial.print("{\"hall_sensor\": ");
+  Serial.print("{\"status\": \"running\"");
+  Serial.print(", \"hall_sensor\": ");
   Serial.print(analogRead(A0));
   Serial.print(", \"water_temp\": ");
   Serial.print(temperature);
